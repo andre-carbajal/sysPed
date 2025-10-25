46cade709c9c885a47b0820ff8f33e8f02aeaf75
andre-carbajal <carbajalvargasandre@gmail.com>
2025-10-25 15:08:32 -0500

    refactor: replace ListCrudRepository with JpaRepository in repository interfaces

--- src/main/java/net/andrecarbajal/sysped/repository/RolRepository.java
+++ src/main/java/net/andrecarbajal/sysped/repository/RolRepository.java
@@ -1,12 +1,12 @@
 package net.andrecarbajal.sysped.repository;
 
 import net.andrecarbajal.sysped.model.Rol;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RolRepository extends ListCrudRepository<Rol, Long> {
+public interface RolRepository extends JpaRepository<Rol, Long> {
     Optional<Rol> findByName(String name);
 }
